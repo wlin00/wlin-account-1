6d2e2d0d4a704be01fe2ddc05c84f37451d94836
wlin00 <616294069@qq.com>
2022-10-28 22:07:11 +0800

    feat: 代码重构

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
+import { routes } from './config/routes'
 import { createApp } from 'vue'
 import { App } from './App'
-import { router } from './router'
+import { createRouter } from 'vue-router'
+import { history } from './shared/history'
+import '@svgstore';
+
+const router = createRouter({ history, routes })
 
 const app = createApp(App)
 app.use(router)
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+// @ts-nocheck
+import { svgstore } from './src/vite_plugins/svgstore';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +11,8 @@ export default defineConfig({
     vueJsx({
       transformOn: true, // 支持jsx中写on来配置点击时间
       mergeProps: true, // 自动把class、style、onClick等属性绑定到子组件的root元素
-    })
+    }),
+    svgstore(),
   ],
   server: {
     host: '0.0.0.0'
