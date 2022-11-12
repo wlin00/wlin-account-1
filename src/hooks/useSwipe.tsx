392af61e49e98a751ce85d5aad98d926c1fbe680
wlin00 <wlin0z@163.com>
2022-11-12 14:29:19 +0800

    add: useSwipe自定义hook用于监听用户手指滑动事件

--- src/hooks/useSwipe.tsx
+++ src/hooks/useSwipe.tsx
@@ -6,52 +6,81 @@ type Point = {
 }
 
 export const useSwipe = (element: Ref<HTMLElement | null>) => {
-  const start = ref<Point | null>(null)
-  const end = ref<Point | null>(null)
-  const swiping = ref(false)
+  // useSwipe 自定义hook，用于监听传入的dom节点的手指滑动事件；
+  // 向外部返回一个计算属性的《方向》、《滑动结束标识符》、《滑动位移》等信息的对象
+  const start = ref<Point>() // typeof [Point, undefined]
+  const end = ref<Point>() // typeof [Point, undefined]
+  const swiping = ref<boolean>(false)
+
+  // 计算属性： 当前手指滑动发生时的x，y偏移（笛卡尔坐标）
   const distance = computed(() => {
-    if (!start.value || !end.value) { return null }
+    if (!start.value || !end.value) {
+      return null
+    }
     return {
       x: end.value.x - start.value.x,
       y: end.value.y - start.value.y,
     }
   })
-  const direction = computed(() => {
-    if (!distance.value) { return '' }
+
+  // 计算属性： 当前手指滑动发生时，根据结束下标 - 开始下标，来推导出滑动方向
+  const direction = computed<string>(() => {
+    if (!distance.value) {
+      return ''
+    }
+    // 根据当前偏移的y和x的绝对值（即两个坐标轴上的投影），来判定当前方向是y轴还是x轴
     const { x, y } = distance.value
     if (Math.abs(x) > Math.abs(y)) {
       return x > 0 ? 'right' : 'left'
     } else {
       return y > 0 ? 'down' : 'up'
     }
+
   })
-  const onStart = (e: TouchEvent) => {
-    swiping.value = true
-    end.value = start.value = { x: e.touches[0].screenX, y: e.touches[0].screenY }
-  }
-  const onMove = (e: TouchEvent) => {
-    if (!start.value) { return }
-    end.value = { x: e.touches[0].screenX, y: e.touches[0].screenY, }
-  }
-  const onEnd = (e: TouchEvent) => {
-    swiping.value = false
+
+  // 手指滑动开始 - 记录开始坐标
+  const handleTouchStart = (e: TouchEvent) => {
+    start.value = {
+      x: e.touches[0].clientX, // touches[0]获取到第一根手指的活动
+      y: e.touches[0].clientY, // touches[0]获取到第一根手指的活动
+    }
+    swiping.value = true // 开始滑动标识符
   }
 
+  // 手指滑动过程中
+  const handleTouchMove = (e: TouchEvent) => {
+    end.value = {
+      x: e.touches[0].clientX,
+      y: e.touches[0].clientY,
+    }
+  }
+
+  // 手指滑动结束
+  const handleTouchEnd = () => {
+    swiping.value = false // 结束滑动标识符
+  }
+  
   onMounted(() => {
-    if (!element.value) { return }
-    element.value.addEventListener('touchstart', onStart)
-    element.value.addEventListener('touchmove', onMove)
-    element.value.addEventListener('touchend', onEnd)
+    if (!element.value) {
+      return
+    }
+    element.value.addEventListener('touchstart', handleTouchStart)
+    element.value.addEventListener('touchmove', handleTouchMove)
+    element.value.addEventListener('touchend', handleTouchEnd)
   })
+
   onUnmounted(() => {
-    if (!element.value) { return }
-    element.value.removeEventListener('touchstart', onStart)
-    element.value.removeEventListener('touchmove', onMove)
-    element.value.removeEventListener('touchend', onEnd)
+    if (!element.value) {
+      return
+    }
+    element.value.removeEventListener('touchstart', handleTouchStart)
+    element.value.removeEventListener('touchmove', handleTouchMove)
+    element.value.removeEventListener('touchend', handleTouchEnd)
   })
+
   return {
-    swiping,
-    direction,
     distance,
+    direction,
+    swiping,
   }
 }
