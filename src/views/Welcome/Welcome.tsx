76c3f644e48ae916b3156caf64c33385805de3ab
wlin00 <wlin0z@163.com>
2022-11-13 00:33:45 +0800

    fix: type constraint

--- src/views/Welcome/Welcome.tsx
+++ src/views/Welcome/Welcome.tsx
@@ -3,23 +3,28 @@ import { RouteLocationNormalizedLoaded, RouterView, useRouter, useRoute } from '
 import { useSwipe } from '../../hooks/useSwipe';
 import { throttle } from '../../utils';
 import s from './Welcome.module.scss'
+interface IRouteMap {
+  welcome1: string;
+  welcome2: string;
+  welcome3: string;
+  welcome4: string;
+}
 export const Welcome = defineComponent({
   setup: (props, context) => {
     const main = ref<HTMLElement | null>(null)
     const router = useRouter()
     const route = useRoute()
     const { direction, distance, swiping } = useSwipe(main)
-    const routeMap: Record<string, string> = {
+    const routeMap: Record<keyof IRouteMap, string> = {
       'welcome1': '/welcome/2',
       'welcome2': '/welcome/3',
       'welcome3': '/welcome/4',
       'welcome4': '/start',
     }
     const push = throttle(() => { // 手指滑动事件节流，节流时间为动画的时间
-      const routeName = String(route.name) ?? 'welcome1' // 将routeName类型约束在string，若是symbol则进行toString，若是null/undefined则给默认值
+      const routeName = route.name as keyof IRouteMap // 将routeName类型约束在string，若是symbol则进行toString，若是null/undefined则给默认值
       const path = routeMap[routeName]
       router.push(path)
- 
     }, 500)
     watchEffect(() => {
       if (swiping.value && direction.value === 'left') { // 当前若进行手指左滑，则动画切换至下一个页面
