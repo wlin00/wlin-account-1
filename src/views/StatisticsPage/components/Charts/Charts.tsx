a3bf2e50a96ef61711f4990d0ea79d2b8e4b640b
wlin00 <wlin0z@163.com>
2022-12-12 01:40:16 +0800

    add: chart page

--- src/views/StatisticsPage/components/Charts/Charts.tsx
+++ src/views/StatisticsPage/components/Charts/Charts.tsx
@@ -1,9 +1,11 @@
 
 
-import { defineComponent, onMounted, PropType, ref } from 'vue';
+import { defineComponent, PropType, ref } from 'vue';
 import { FormItem } from '../../../../components/Form/Form';
 import s from './Charts.module.scss';
-import * as echarts from 'echarts';
+import { LineChart } from '../LineChart/LineChart';
+import { PieChart } from '../PieChart/PieChart';
+import { Bars } from '../Bars/Bars';
 
 export const Charts = defineComponent({
   props: {
@@ -18,73 +20,16 @@ export const Charts = defineComponent({
   },
   setup: (props, context) => {
     const currentSelect = ref('expenses')
-    const refDiv = ref<HTMLElement>()
-    const refDiv2 = ref<HTMLElement>()
-    onMounted(() => {
-      const dom = refDiv.value!
-      // 初始化echarts实例
-      let myChart = echarts.init(dom)
-      // 绘制图表
-      myChart.setOption({
-        grid: [
-          { left: 0, top: 0, right: 0, bottom: 20 }
-        ],
-        xAxis: {
-          type: 'category',
-          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-        },
-        yAxis: {
-          type: 'value'
-        },
-        series: [
-          {
-            data: [150, 230, 224, 218, 135, 147, 260],
-            type: 'line'
-          }
-        ]
-      });
-    })
-    onMounted(() => {
-      const dom = refDiv2.value!
-      // 基于准备好的dom，初始化echarts实例
-      var myChart = echarts.init(dom);
-      // 绘制图表
-      const option = {
-        grid: [
-          { left: 0, top: 0, right: 0, bottom: 20 }
-        ],
-        series: [
-          {
-            name: 'Access From',
-            type: 'pie',
-            radius: '50%',
-            data: [
-              { value: 1048, name: 'Search Engine' },
-              { value: 735, name: 'Direct' },
-              { value: 580, name: 'Email' },
-              { value: 484, name: 'Union Ads' },
-              { value: 300, name: 'Video Ads' }
-            ],
-            emphasis: {
-              itemStyle: {
-                shadowBlur: 10,
-                shadowOffsetX: 0,
-                shadowColor: 'rgba(0, 0, 0, 0.5)'
-              }
-            }
-          }
-        ]
-      };
-      myChart.setOption(option);
-    })
+
     return () => (
       <div class={s.wrapper}>
         <FormItem label='类型' type="select" options={[
           { value: 'expenses', text: '支出' },
           { value: 'income', text: '收入' }
         ]} v-model={currentSelect.value} />
-        <div style={{marginTop: '30px'}} ref={refDiv} class={s.demo}></div>
-        <div ref={refDiv2} class={s.demo2}></div>
+        <LineChart style={{ marginTop: '40px' }} />
+        <PieChart />
+        <Bars />
       </div>
     )
   }
---
+++ src/views/StatisticsPage/components/PieChart/PieChart.tsx
@@ -0,0 +1,45 @@
+import { defineComponent, onMounted, PropType, ref } from 'vue';
+import s from './PieChart.module.scss';
+import * as echarts from 'echarts'
+
+export const PieChart = defineComponent({
+  setup: (props, context) => {
+    const refDiv = ref<HTMLDivElement>()
+    onMounted(() => {
+      const dom = refDiv.value!
+      // 基于准备好的dom，初始化echarts实例
+      var myChart = echarts.init(dom);
+      // 绘制图表
+      const option = {
+        grid: [
+          { left: 0, top: 0, right: 0, bottom: 20 }
+        ],
+        series: [
+          {
+            name: 'Access From',
+            type: 'pie',
+            radius: '50%',
+            data: [
+              { value: 1048, name: 'Search Engine' },
+              { value: 735, name: 'Direct' },
+              { value: 580, name: 'Email' },
+              { value: 484, name: 'Union Ads' },
+              { value: 300, name: 'Video Ads' }
+            ],
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+              }
+            }
+          }
+        ]
+      };
+      myChart.setOption(option);
+    })
+    return () => (
+      <div ref={refDiv} class={s.wrapper}></div>
+    )
+  }
+})
